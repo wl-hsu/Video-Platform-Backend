cca12aba51fb0961fa12438f98241cfaaccffb5e
wl-hsu <wlhsu.github@gmail.com>
2022-12-11 07:34:12 -0500

    setup exception handler, Json message converter, encryption

---
+++ video-platform-api/src/main/java/com/video/platform/api/DemoApi.java
@@ -0,0 +1,18 @@
+package com.video.platform.api;
+
+import com.video.platform.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoApi {
+
+    @Autowired
+    private DemoService demoService;
+
+    @GetMapping("/query")
+    public Long query(Long id){
+        return demoService.query(id);
+    }
+}
---
+++ video-platform-service/src/main/java/com/video/platform/service/DemoService.java
@@ -0,0 +1,15 @@
+package com.video.platform.service;
+
+import com.video.platform.dao.DemoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    @Autowired
+    private DemoDao demoDao;
+
+    public Long query(Long id){
+        return demoDao.query(id);
+    }
+}
