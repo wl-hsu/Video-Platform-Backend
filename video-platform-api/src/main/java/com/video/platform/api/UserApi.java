766b7334c6bdfabb5f991f1ba52dfba84d7faa63
wl-hsu <wlhsu.github@gmail.com>
2022-12-11 14:32:52 -0500

    add user register function

---
+++ video-platform-api/src/main/java/com/video/platform/api/UserApi.java
@@ -0,0 +1,32 @@
+package com.video.platform.api;
+
+import com.video.platform.domain.JsonResponse;
+import com.video.platform.domain.User;
+import com.video.platform.service.UserService;
+import com.video.platform.service.util.RSAUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+public class UserApi {
+
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping("/rsa-pks")
+    public JsonResponse<String> getRsaPublicKey(){
+        String pk = RSAUtil.getPublicKeyStr();
+        return new JsonResponse<>(pk);
+    }
+
+    @PostMapping("/users")
+    public JsonResponse<String> addUser(@RequestBody User user) {
+        userService.addUser(user);
+        return JsonResponse.success();
+    }
+
+    
+}
---
+++ video-platform-dao/src/main/java/com/video/platform/domain/constant/UserConstant.java
@@ -0,0 +1,14 @@
+package com.video.platform.domain.constant;
+
+public interface UserConstant {
+    public static final String GENDER_MALE = "0";
+
+    public static final String GENDER_FEMALE = "1";
+
+    public static final String GENDER_UNKNOWN = "2";
+
+    public static final String DEFAULT_BIRTH = "2000-01-01";
+
+    public static final String DEFAULT_NICK = "JOHN DOE";
+
+}
