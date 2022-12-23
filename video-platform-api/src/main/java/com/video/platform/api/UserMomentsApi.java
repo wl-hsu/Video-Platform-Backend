8af40eb6a5ecd7ebb76f02ed03b0c9ca3ca45a60
wl-hsu <wlhsu.github@gmail.com>
2022-12-23 17:59:41 -0500

    publish user moment(story) and receive following user moment

---
+++ video-platform-api/src/main/java/com/video/platform/api/UserMomentsApi.java
@@ -0,0 +1,40 @@
+package com.video.platform.api;
+
+import com.video.platform.api.support.UserSupport;
+import com.video.platform.domain.JsonResponse;
+import com.video.platform.domain.UserMoment;
+import com.video.platform.service.UserMomentsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserMomentsApi {
+
+    @Autowired
+    private UserMomentsService userMomentsService;
+
+    @Autowired
+    private UserSupport userSupport;
+
+
+    @PostMapping("/user-moments")
+    public JsonResponse<String> addUserMoments(@RequestBody UserMoment userMoment) throws Exception {
+        Long userId = userSupport.getCurrentUserId();
+        userMoment.setUserId(userId);
+        userMomentsService.addUserMoments(userMoment);
+        return JsonResponse.success();
+    }
+
+    @GetMapping("/user-subscribed-moments")
+    public JsonResponse<List<UserMoment>> getUserSubscribedMoments(){
+        Long userId = userSupport.getCurrentUserId();
+        List<UserMoment> list = userMomentsService.getUserSubscribedMoments(userId);
+        return new JsonResponse<>(list);
+    }
+
+}
---
+++ video-platform-service/src/main/java/com/video/platform/service/UserMomentsService.java
@@ -0,0 +1,45 @@
+package com.video.platform.service;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.video.platform.dao.UserMomentsDao;
+import com.video.platform.domain.UserMoment;
+import com.video.platform.domain.constant.UserMomentsConstant;
+import com.video.platform.service.util.RocketMQUtil;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class UserMomentsService {
+
+    @Autowired
+    private UserMomentsDao userMomentsDao;
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public void addUserMoments(UserMoment userMoment) throws Exception {
+        userMoment.setCreateTime(new Date());
+        userMomentsDao.addUserMoments(userMoment);
+        DefaultMQProducer producer = (DefaultMQProducer)applicationContext.getBean("momentsProducer");
+        Message msg = new Message(UserMomentsConstant.TOPIC_MOMENTS, JSONObject.toJSONString(userMoment).getBytes(StandardCharsets.UTF_8));
+        RocketMQUtil.syncSendMsg(producer, msg);
+    }
+
+    public List<UserMoment> getUserSubscribedMoments(Long userId) {
+        String key = "subscribed-" + userId;
+        String listStr = redisTemplate.opsForValue().get(key);
+        return JSONArray.parseArray(listStr, UserMoment.class);
+    }
+}
