622585ef3bc2c247bf7ba125009603e2c7e44607
wl-hsu <wlhsu.github@gmail.com>
2023-02-21 00:23:45 -0500

    upgrade to dual token and add refresh, access token

--- video-platform-dao/src/main/java/com/video/platform/dao/UserDao.java
+++ video-platform-dao/src/main/java/com/video/platform/dao/UserDao.java
@@ -1,12 +1,14 @@
 package com.video.platform.dao;
 
 import com.alibaba.fastjson.JSONObject;
+import com.video.platform.domain.RefreshTokenDetail;
 import com.video.platform.domain.User;
 import com.video.platform.domain.UserInfo;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -36,4 +38,13 @@ public interface UserDao {
     Integer pageCountUserInfos(Map<String, Object> params);
 
     List<UserInfo> pageListUserInfos(JSONObject params);
+
+    Integer deleteRefreshToken(@Param("refreshToken") String refreshToken,
+                               @Param("userId") Long userId);
+
+    Integer addRefreshToken(@Param("refreshToken")String refreshToken,
+                            @Param("userId") Long userId,
+                            @Param("createTime") Date createTime);
+
+    RefreshTokenDetail getRefreshTokenDetail(String refreshToken);
 }
